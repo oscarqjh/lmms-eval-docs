45e21927c02fa0f48e58195681cb1b2789c4a0e2
oscarqjh <oscar.jh9@gmail.com>
2026-02-24 16:02:26 +0800

    style: aligned styling with main webpage

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -9,24 +9,24 @@ export default function Layout({ children }: { children: ReactNode }) {
     <DocsLayout
       sidebar={{
         tabs: [
-          {
-            title: "Models",
-            description: "Our models",
-            url: "/docs/models",
-            icon: <Boxes color="#ad6dd0" className="w-5 h-5" />,
-          },
+          // {
+          //   title: "Models",
+          //   description: "Our models",
+          //   url: "/docs/models",
+          //   icon: <Boxes color="#ad6dd0" className="w-5 h-5" />,
+          // },
           {
             title: "lmms-eval",
             description: "Evaluation framework",
             url: "/docs/lmms-eval",
             icon: <ChartColumnIcon color="#4aafdeff" className="w-5 h-5" />,
           },
-          {
-            title: "lmms-engine",
-            description: "Training framework",
-            url: "/docs/lmms-engine",
-            icon: <Wrench color="#f59e0b" className="w-5 h-5" />,
-          },
+          // {
+          //   title: "lmms-engine",
+          //   description: "Training framework",
+          //   url: "/docs/lmms-engine",
+          //   icon: <Wrench color="#f59e0b" className="w-5 h-5" />,
+          // },
         ],
       }}
       tree={source.pageTree}
